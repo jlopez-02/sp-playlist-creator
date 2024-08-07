5f8210ebe178958b3b167f51f76b96767c4bffa0
juanlopesp <102731662+juanlopesp@users.noreply.github.com>
2024-08-07 20:33:04 +0200

    Cambios en la url

--- src/components/SpComponents/SpotifyAuth.jsx
+++ src/components/SpComponents/SpotifyAuth.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {REDIRECT_URI, AUTH_ENDPOINT, RESPONSE_TYPE, SCOPES} from './../../misc/sp_config';
 
 const SpotifyAuth = ({type = "login", setUser}) => {
   const CLIENT_ID = 'cd94803c352d41ebb5ccb110b98f6e89';
-  const REDIRECT_URI = 'http://localhost:3000/callback';
-  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
-  const RESPONSE_TYPE = 'code';
-  const SCOPES = "user-read-private user-read-email playlist-read-private user-top-read playlist-modify-public playlist-modify-private"
+  //const REDIRECT_URI = 'http://localhost:3000/callback';
+  //const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+  //const RESPONSE_TYPE = 'code';
+  //const SCOPES = "user-read-private user-read-email playlist-read-private user-top-read playlist-modify-public playlist-modify-private"
 
   const handleLogin = () => {
     window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}&show_dialog=true`;
---
+++ src/misc/setup.js
@@ -0,0 +1,2 @@
+//export const PAGE_URL = 'http://localhost:3000';
+export const PAGE_URL = 'https://jlopez-02.github.io/';
